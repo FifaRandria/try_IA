12b4a07bd02b90129ac9c5f15d02415e62097085
FifaRandria <fifarandria92@gmail.com>
2026-04-17 12:03:13 +0200

    resolve problem concern deploiment

--- my-app/lib/prisma.js
+++ my-app/lib/prisma.js
@@ -1,4 +1,7 @@
 import { PrismaClient } from '@/app/generated/prisma/client'
+
+// import { PrismaClient } from '@prisma/client'
+
 import { PrismaPg } from '@prisma/adapter-pg'
 import pg from 'pg'
 
